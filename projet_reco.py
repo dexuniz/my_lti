0c1b3adc568a07c9b35894e63ed3c8d71b7477fb
polo31 <hack2day.paul@gmail.com>
2017-01-04 18:57:41 +0100

    0.04

--- projet_reco.py
+++ projet_reco.py
@@ -3,7 +3,8 @@
 import os
 import sys
 import requests
-from flask import Flask
+from flask import Flask, redirect, url_for, send_from_directory, 
+                  render_template
 from flask import render_template
 from flask.ext.wtf import Form
 from wtforms import IntegerField, BooleanField
@@ -111,7 +112,43 @@ def photo(lti=lti):
 	reload(sys)  
 	sys.setdefaultencoding('utf8')
 	return render_template('photo.html', form=form, photo=photo)
+	
+UPLOAD_FOLDER = './pictures/'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+@app.route('/download', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = "http://wallpaper-gallery.net/images/h-o-t-pictures/h-o-t-pictures-22.jpg"
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file', filename=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form action="" method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    '''
+@app.route('/show/<filename>')
+def uploaded_file(filename):
+    filename = 'https://mighty-refuge-99683.herokuapp.com/upload/' + filename
+    return render_template('image.html', filename=filename)
+
+@app.route('/uploads/<filename>')
+def send_file(filename):
+    return send_from_directory(UPLOAD_FOLDER, filename)
+	
 def set_debugging():
     """ Debuggage du logging
 
