fab9d43b3d0ed5291d9153d1672f919a24e8147f
polo31 <hack2day.paul@gmail.com>
2017-02-03 13:12:03 +0100

    0.06

--- projet_reco.py
+++ projet_reco.py
@@ -5,9 +5,6 @@ import sys
 import requests
 from flask import Flask, redirect,url_for , send_from_directory, render_template
 from flask import render_template
-from flask.ext.wtf import Form
-from wtforms import IntegerField, BooleanField
-from random import randint
 import urllib
 import sqlite3
 from pylti.flask import lti
@@ -19,20 +16,6 @@ app = Flask(__name__)
 app.config.from_object('config')
 
 
-class AddForm(Form): 
-    
-    """ 
-    Utilisé pour le calcul simple du lien start problems
-    
-    
-    Add data from Form
-
-    :param Form:
-    """
-    p1 = IntegerField('p1')
-    p2 = IntegerField('p2')
-    result = IntegerField('result')
-    correct = BooleanField('correct')
 
 
 def error(exception=None):
@@ -102,14 +85,38 @@ def grade(lti=lti):
     form.correct.data = correct
     #lti.post_grade(1 if correct else 0)
     return render_template('grade.html', form=form)
-
-@app.route('/photo', methods=['GET', 'POST'])
-@lti(request='session', error=error, app=app)
-def photo(lti=lti):
 	
+	
+@app.route('/teacher',methods=['GET','POST'])
+@lti(request='session', error=error, app=app)
+def teachers_class(lti=lti):
+	id = lti.user_id
 	myDB = MySQLdb.connect(host="127.0.0.1",port=3306,user="root",passwd="",db="moodle")
 	cHandler = myDB.cursor()
+	cHandler.execute("SELECT DISTINCT u.id AS userid, u.lastname AS lastname, c.id AS courseid, c.fullname AS fullname\
+	FROM mdl_user u\
+	JOIN mdl_user_enrolments ue ON ue.userid = u.id\
+	JOIN mdl_enrol e ON e.id = ue.enrolid\
+	JOIN mdl_role_assignments ra ON ra.userid = u.id\
+	JOIN mdl_context ct ON ct.id = ra.contextid AND ct.contextlevel = 50\
+	JOIN mdl_course c ON c.id = ct.instanceid AND e.courseid = c.id\
+	JOIN mdl_role r ON r.id = ra.roleid AND r.shortname = 'teacher'\
+	WHERE e.status = 0 AND u.suspended = 0 AND u.deleted = 0\
+	AND (ue.timeend = 0 OR ue.timeend > NOW()) AND ue.status = 0 AND u.id = %s", id)
+	results = cHandler.fetchall()
+	return render_template('photo.html', results=results)
+	
+	
+@app.route('/students_related',methods=['GET','POST'])
+@lti(request='session', error=error, app=app)
+def get_studs(lti=lti):
+
+	course_number=4
+	myDB = MySQLdb.connect(host="127.0.0.1",port=3306,user="root",passwd="",db="moodle")
+	cHandler = myDB.cursor()
+	#cHandler.execute("SELECT 
 	#cHandler.execute("SELECT defaultgroupingid FROM mdl_course WHERE fullname='Recommendation'")
+	#Liste des étudiants du cours sous la forme (userid, lastname, course id)
 	cHandler.execute("SELECT DISTINCT u.id AS userid, u.lastname AS lastname, c.id AS courseid\
 	 FROM mdl_user u\
 	 JOIN mdl_user_enrolments ue ON ue.userid = u.id\
@@ -119,12 +126,39 @@ def photo(lti=lti):
 	 JOIN mdl_course c ON c.id = ct.instanceid AND e.courseid = c.id\
 	 JOIN mdl_role r ON r.id = ra.roleid AND r.shortname = 'student'\
 	 WHERE e.status = 0 AND u.suspended = 0 AND u.deleted = 0\
-	 AND (ue.timeend = 0 OR ue.timeend > NOW()) AND ue.status = 0 AND courseid ='4'")
+	 AND (ue.timeend = 0 OR ue.timeend > NOW()) AND ue.status = 0 AND courseid = %s", course_number)
 	#cHandler.execute("SELECT id FROM mdl_user_enrolments WHERE mdl_user_enrolments.enrolid = '7L'")
 	#cHandler.execute("SELECT lastname FROM mdl_user WHERE mdl_user.id = 2 OR mdl_user.id = 3")
 	#cHandler.execute("SELECT * FROM information_schema.tables WHERE TABLE_TYPE='BASE TABLE'")
 	results = cHandler.fetchall()
-	return render_template('photo.html', form=form, results=results)
+	return render_template('photo.html', results=results)	
+	
+
+@app.template_filter('get_students_from_course')
+@lti(request='session', error=error, app=app)
+def get_studs(course_number):
+
+	course_number=(course_number,)
+	myDB = MySQLdb.connect(host="127.0.0.1",port=3306,user="root",passwd="",db="moodle")
+	cHandler = myDB.cursor()
+	#cHandler.execute("SELECT 
+	#cHandler.execute("SELECT defaultgroupingid FROM mdl_course WHERE fullname='Recommendation'")
+	#Liste des étudiants du cours sous la forme (userid, lastname, course id)
+	cHandler.execute("SELECT DISTINCT u.id AS userid, u.lastname AS lastname, c.id AS courseid\
+	 FROM mdl_user u\
+	 JOIN mdl_user_enrolments ue ON ue.userid = u.id\
+	 JOIN mdl_enrol e ON e.id = ue.enrolid\
+	 JOIN mdl_role_assignments ra ON ra.userid = u.id\
+	 JOIN mdl_context ct ON ct.id = ra.contextid AND ct.contextlevel = 50\
+	 JOIN mdl_course c ON c.id = ct.instanceid AND e.courseid = c.id\
+	 JOIN mdl_role r ON r.id = ra.roleid AND r.shortname = 'student'\
+	 WHERE e.status = 0 AND u.suspended = 0 AND u.deleted = 0\
+	 AND (ue.timeend = 0 OR ue.timeend > NOW()) AND ue.status = 0 AND courseid = ?", course_number)
+	#cHandler.execute("SELECT id FROM mdl_user_enrolments WHERE mdl_user_enrolments.enrolid = '7L'")
+	#cHandler.execute("SELECT lastname FROM mdl_user WHERE mdl_user.id = 2 OR mdl_user.id = 3")
+	#cHandler.execute("SELECT * FROM information_schema.tables WHERE TABLE_TYPE='BASE TABLE'")
+	results = cHandler.fetchall()
+	return render_template('photo.html', results=results)
 
 
 def set_debugging():
