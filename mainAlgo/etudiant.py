9995d511d6e04f6c1ccf60ac04321df244463cdc
Paul Mougin <Paul Mougin>
2017-04-24 11:53:24 +0200

    évaluation compétences

--- mainAlgo/etudiant.py
+++ mainAlgo/etudiant.py
@@ -1,19 +1,26 @@
+
 class Etudiant():
     """Représentation d'un étudiant et de ses capacités    """
 
-    def __init__(self, prenom, nom, matriceCompetences=[]):
+    def __init__(self, prenom, nom, niveauxCompetences=[], capacite=0):
         self.prenom = prenom
         self.nom = nom
-        self.matriceCompetences = matriceCompetences
-        self.quiz = []
+        self.niveauxCompetences = niveauxCompetences ## [theta_1, theta_2, ...]
+        self.capacite = capacite
+        self.questionsRepondues = {} ## {question.idNb: -1 non repondue, 0 faux ou 1 juste, ...}
         
-    def niveauCompetences(self, competenceId):
-        return self.matriceCompetences[competenceId]
+    def getNiveau(self, competenceId):
+        return self.niveauxCompetences[competenceId]
         
-    def actualiserNiveau(self, competenceId, niveau):
-        self.matriceCompetences[competenceId, niveau]
+    def setNiveaux(self, niveaux):
+        self.niveauxCompetences = niveaux
+ 
+    def setCapacite(self, capacite):
+        self.capacite = capacite
+        
+
+    def getCapacites(self):
+        return self.capacite, self.niveauxCompetences
     
-    def ajouterQuizz(self, quiz):
-        self.quiz.append(quiz)      
-        
+
         
